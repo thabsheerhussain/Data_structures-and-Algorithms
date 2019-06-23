49c0c9935a1cea4b276ff874800f0f8672abb319
Ashwani Thakur <ashwani@crowdanalytix.net>
2019-06-23 12:10:36 +0530

    implemented stack in python

---
+++ stacki.py
@@ -0,0 +1,30 @@
+class Stack:
+    def __init__(self):
+        self.items=[]
+
+
+    def isEmpty(self):
+        return self.items ==[]
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+
+
+s = Stack()
+s.push(1)
+s.push(2)
+s.push(3)
+print(s.peek())
+s.pop()
+print(s.peek())
+print(s.size())
