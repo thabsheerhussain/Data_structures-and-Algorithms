8875ce566c0046d622531a3e3787fde32c6a4eca
Ashwani Thakur <ashwani@crowdanalytix.net>
2019-06-23 23:06:35 +0530

    updated linked list

--- ilinkedlist.py
+++ ilinkedlist.py
@@ -1,54 +1,92 @@
 class Node:
-    def __init__(self,init_data):
-        self.data = init_data
+    def __init__(self,initdata):
+        self.data = initdata
         self.next = None
-    
+
     def getData(self):
         return self.data
 
     def getNext(self):
         return self.next
 
-    def setData(self,ndata):
-        self.data = ndata
+    def setData(self,newdata):
+        self.data = newdata
 
-    def setNext(self,nnext):
-        self.next = nnext
+    def setNext(self,newnext):
+        self.next = newnext
 
 
-class linked_list:
+class UnorderedList:
 
     def __init__(self):
-        self.head=None
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
 
     def add(self,item):
         temp = Node(item)
         temp.setNext(self.head)
-        self.head=temp
+        self.head = temp
 
     def size(self):
         current = self.head
-        count =0
+        count = 0
         while current != None:
-            count+=1
+            count = count + 1
             current = current.getNext()
 
         return count
 
     def search(self,item):
-        current=self.head
+        current = self.head
         found = False
-        while current !=None and not found:
-            if current.getData()==item:
+        while current != None and not found:
+            if current.getData() == item:
                 found = True
             else:
                 current = current.getNext()
 
         return found
 
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
 
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
 
+mylist = UnorderedList()
 
+mylist.add(31)
+mylist.add(77)
+mylist.add(17)
+mylist.add(93)
+mylist.add(26)
+mylist.add(54)
 
+print(mylist.size())
+print(mylist.search(93))
+print(mylist.search(100))
 
+mylist.add(100)
+print(mylist.search(100))
+print(mylist.size())
+
+mylist.remove(54)
+print(mylist.size())
+mylist.remove(93)
+print(mylist.size())
+mylist.remove(31)
+print(mylist.size())
+print(mylist.search(93))
 
