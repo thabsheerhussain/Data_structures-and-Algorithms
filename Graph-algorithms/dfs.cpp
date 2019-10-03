5ed71403b0dc58e3c4f6996f237f329b121fe84a
Suyash Yadav <suyashky@gmail.com>
2019-10-03 19:34:15 +0530

    dfs with adjacency list in cpp

---
+++ Graph-algorithms/dfs.cpp
@@ -0,0 +1,45 @@
+/*
+    "DFS IMPLEMENTATION USING RECUSTION IN CPP WITH ADJACENCY LIST"
+
+    INPUT FORMAT:
+    1. Input number of edges and nodes.
+    2. Enter info of each edge i.e. goes from a to b 
+    3. x is starting vertex for DFS
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(vector<vector<int>> &adj, vector<bool> visited, int x)
+{
+	visited[x] = true;
+	cout<<x<<"\n";
+
+	for(int i=0 ; i<adj[x].size() ; i++)
+    {
+        if(visited[adj[x][i]]==false)
+        {
+               dfs(adj,visited,adj[x][i]);
+        }
+    }
+}
+
+int main()
+{
+    int edges,nodes;
+    cin>>edges>>nodes;
+    
+    vector<vector<int>> adj(nodes+1);
+    
+    int a,b;
+    for(int i=0 ; i<edges ; i++)
+    {    
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    vector<bool> visited(nodes+1,false);
+
+    dfs(adj,visited,1);
+} 
